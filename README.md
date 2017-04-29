ff56f785c7aefb802204aa3d04c580a52479ec48
magna <magna.1989@gmail.com>
2017-04-29 22:18:20 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This script will start Windows Services
---
+++ servicestarter.ps1
@@ -0,0 +1,18 @@
+﻿#populate the service names here
+$services = @("SNMP Trap");
+
+#
+foreach ($service in $services)
+{
+    $serviceObject = Get-Service $service
+
+    if ($serviceObject.Status -eq 'Stopped')
+    {
+        Start-Service $service
+        Write-Host "Started service: "$service -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host $service " is already running" -ForegroundColor Magenta
+    }
+}
